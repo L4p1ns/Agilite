d03ce6542f2641c88a45b437d925654c72493c6a
bayendembaseye <bayendemba.seye@koonda.fr>
2019-05-08 08:47:55 +0000

    Recherche.

--- src/main/java/com/iam/gesconsspring/dao/ProjetRepository.java
+++ src/main/java/com/iam/gesconsspring/dao/ProjetRepository.java
@@ -1,13 +1,23 @@
 package com.iam.gesconsspring.dao;
 
 import com.iam.gesconsspring.entities.Projet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.List;
+
+@CrossOrigin("*")
 @RepositoryRestResource
 @Repository
 public interface ProjetRepository extends JpaRepository<Projet, Long> {
-
+    @RestResource(path = "/byNom")
+    public List<Projet> findByNomContains(@Param("mc") String nom);
+    @RestResource(path = "/byNomPage")
+    public Page<Projet> findByNomContains(@Param("mc") String nom, Pageable pageable);
 }
